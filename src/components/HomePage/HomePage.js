3faada4f5a1fd90d26b665d94983bc44843b4ac4
Jack Chang <Jackwe@zhangdeMBP.modem>
2020-02-07 17:44:55 +1100

    react hooks

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './HomePage.css';
+import { useState, useEffect } from 'react';
+
+
+function HomePage() {
+    const [items, setItems] = useState([]);
+    const [initialized, setInitialized] = useState(false);
+    const [totalHits, setToralHits] = useState(0)
+
+return (
+        <div className="HomePage">
+        
+        </div>
+    );
+}
+export default HomePage; 
---
+++ src/components/ImageSearch/ImageSearch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+
+function ImageSearchPage(){
+    return (
+        <div className="ImageSearch">
+
+        </div>
+    )
+}
