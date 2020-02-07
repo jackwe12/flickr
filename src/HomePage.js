dcd0af37c56611c0688abf92737934fedb5fa29f
Jack Chang <Jackwe@zhangdeMBP.modem>
2020-02-07 17:22:29 +1100

    add home page

---
+++ src/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.css';
+
+
+function HomePage() {
+  
+return (
+        <div className="HomePage">
+        
+        </div>
+    );
+}
+export default HomePage;
