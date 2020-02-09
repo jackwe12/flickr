943bcd19509d2cfcf009bb02bf170293faa6dab3
Jack Chang <Jackwe@zhangdeMBP.modem>
2020-02-09 23:33:03 +1100

    rearrange

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { Router, Route } from "react-router-dom";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav'
